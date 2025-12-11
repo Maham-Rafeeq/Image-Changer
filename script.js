aa198f309e2e6fc60d89e324afebeb43d3f86cfc
Maham Rafeeq <codewithmaham@gmail.com>
2025-12-11 02:55:48 -1200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let img = document.getElementById("image");
+let btn = document.getElementById("btn");
+
+let ison = false;
+function clickme() {
+    if (ison === false) {
+        ison = true;
+        btn.innerText = "Again";
+        img.src = "./Images/img4.jpg";
+
+    }
+    else{
+        ison = false;
+      img.src ="./Images/img2.jpeg";
+      btn.innerText ="click";
+    }
+}
